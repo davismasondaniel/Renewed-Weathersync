da8ed9f016fb91ee43e454381f354ec9c146ebe8
davismasondaniel <108919916+davismasondaniel@users.noreply.github.com>
2025-12-04 18:23:03 +0000

    fix

--- config/weather.lua
+++ config/weather.lua
@@ -2,7 +2,7 @@ return {
 
     useScheduledWeather = true, -- Do you want txAdmin to schedule custom rain and thunder near restart?
     serverDuration = 14, -- How many hours will the server run before restarting?, if a server restarts every 8 hours put this to 9 etc.
-    weatherCycletimer = 30, -- How many minutes between weather changes
+    weatherCycletimer = 10, -- How many minutes between weather changes
 
     timeBetweenRain = 180, -- How many minutes between rain events
     rainAfterRestart = 60, -- How many minutes AFTER a server restart before rain will start to show?
